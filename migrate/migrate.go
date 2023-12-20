f7dda3be50475ad685420f85c19eabafe86273b3
Anas Firly <anasfirly20@gmail.com>
2023-12-20 15:29:57 +0700

    feat: adding migrate.go

---
+++ migrate/migrate.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/anasfirly20/go-codevo/initializers"
+	"github.com/anasfirly20/go-codevo/models"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+	initializers.ConnectDB(&config)
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.User{})
+	fmt.Println("Migration complete")
+}
